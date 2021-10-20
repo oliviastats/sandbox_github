d1a75575159173e2af0de47d3cd240857a4638e8
Olivia Sliz <olivia.sliz@mgb.ch>
2021-10-20 15:07:50 +0000

    finish decision tree model

--- decision_tree/decision_tree_regressor_from_scratch.py
+++ decision_tree/decision_tree_regressor_from_scratch.py
@@ -1,11 +1,25 @@
 import numpy as np
 import pandas as pd
 
+# sources https://github.com/Suji04/ML_from_Scratch/blob/master/decision%20tree%20classification.ipynb
+
+class Node:
+    def __init__(self,  feature_index=None, splitting_value=None, left=None, right=None, info_gain=None, value=None):
+        # for decision node
+        self.feature_index = feature_index
+        self.splitting_value = splitting_value
+        self.left = left
+        self.right = right
+        self.info_gain = info_gain
+        
+        # for leaf node
+        self.value = value
 
 class DecisionTreeClassifier:
     def __init__(self, max_depth=2):
         # stopping conditions
         self.max_depth = max_depth
+        self.root = None
 
     def entropy(self, y):
         """
@@ -62,25 +76,69 @@ class DecisionTreeClassifier:
         subset_right = dataset[dataset[:,feature_index] > splitting_value]
         return subset_left, subset_right
 
-    def determine_optimal_split(self, dataset: np.array):
+    def determine_optimal_split(self, dataset: np.array, feature_indices):
         X = dataset[:, :-1]
         y = dataset[:,-1]
-        number_features = X.shape[1]
         bestsplit = {}
         information_gain = -np.inf
-        for i in range(number_features):
-            feature_values = X[:,i]
+        feature_indices = range(X.shape[1])
+        for index in feature_indices:
+            feature_values = X[:,index]
             for value in feature_values:
-                subset_left, subset_right = self.split_data(dataset, i, value)
+                subset_left, subset_right = self.split_data(dataset, index, value)
                 information_gain_curr = self.information_gain(y, subset_left, subset_right)
                 if information_gain_curr > information_gain:
                     information_gain = information_gain_curr
-                    bestsplit['feature_index'] = i
-                    bestsplit['feature_value'] = value
+                    bestsplit['feature_index'] = index
+                    bestsplit['splitting_value'] = value
                     bestsplit['subset_left'] = subset_left
                     bestsplit['subset_right'] = subset_right
-                    bestsplit['information_gain'] = information_gain_curr
+                    bestsplit['info_gain'] = information_gain_curr
         return bestsplit
+
+    def build_tree(self, dataset, curr_depth=0):
+        ''' recursive function to build the tree ''' 
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        feature_indices = range(X.shape[1])
+        if curr_depth<=self.max_depth:
+            best_split = self.determine_optimal_split(dataset, feature_indices)
+            # check if information gain is positive
+            if best_split["info_gain"]>0:
+                # recur left
+                left_subtree = self.build_tree(best_split["subset_left"], curr_depth+1)
+                # recur rightS
+                right_subtree = self.build_tree(best_split["subset_right"], curr_depth+1)
+                # return decision node
+                return Node(best_split["feature_index"], best_split['splitting_value'], 
+                            left_subtree, right_subtree, best_split["info_gain"])
+        # compute leaf node
+        leaf_value = self.calculate_leaf_value(Y)
+        # return leaf node
+        return Node(value=leaf_value)
+           
+    def calculate_leaf_value(self, Y):
+        ''' function to compute leaf node '''
         
-
-
+        Y = list(Y)
+        return max(Y, key=Y.count)
+ 
+    def fit(self, X, Y):
+        dataset = np.concatenate((X,Y), axis=1)
+        self.root = self.build_tree(dataset)
+        print('finish')
+    
+    def predict(self, X):
+        ''' function to predict new dataset '''
+        
+        preditions = [self.make_prediction(x, self.root) for x in X]
+        return preditions
+    
+    def make_prediction(self, x, tree):
+        ''' function to predict a single data point '''
+        
+        if tree.value!=None: return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val<=tree.splitting_value:
+            return self.make_prediction(x, tree.left)
+        else:
+            return self.make_prediction(x, tree.right)
